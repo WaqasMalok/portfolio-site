4bd602f6d31537bf1f7d71e8d4c4ee286669d741
WaqasMalok <waqasimalok@gmail.com>
2022-10-02 21:29:39 +0500

    add contact form

--- style.js
+++ style.js
@@ -1,11 +1,3 @@
-
-// window.addEventListener('scroll', function () {
-//     let nav = document.querySelector('nav');
-//     nav.classList.toggle("bg", window.scrollY > 0);
-//     let content = document.querySelector('.main');
-//     content.classList.toggle("transform", window.scrollY > 1);
-
-// });
 window.addEventListener('scroll', ()=>{
     let nav = document.querySelector('nav');
     nav.classList.toggle("bg", window.scrollY > 0);
